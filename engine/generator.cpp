cdc0e068a071df89ebf88e8b33bd26b848b873fb
reginox <99781653+reginoxcf@users.noreply.github.com>
2024-03-22 21:26:46 +0700

    Update generator.cpp

--- engine/generator.cpp
+++ engine/generator.cpp
@@ -8,12 +8,15 @@ long long getRandomInteger(long long lowerBound, long long upperBound){
 
 int main(){
 	int n;
+	cout << "Enter number of teams (even):";
 	cin >> n;
-	freopen("teams.txt","w",stdout);
-	cout << n << "\n";
+	ofstream file("teams.txt");
+	file << n << "\n";
 	while(n--){
-		for(int i =0; i< 3; i++) cout << char(getRandomInteger(0, 25)+'A');
-		cout << " ";
-		cout << getRandomInteger(1, 10000) << " " << getRandomInteger(1, 10000) << "\n";
+		for(int i = 0; i < 3; i++) file << char(getRandomInteger(0, 25)+'A');
+		file << " ";
+		file << getRandomInteger(1, 10000) << " " << getRandomInteger(1, 10000) << "\n";
 	}
+	file.close();
+	cout << "Generated " << n << " random teams! Please close this window and run simulator.cpp.";
 }
