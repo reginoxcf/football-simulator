d554f0549ddd68600ff726e8e27c18eb767e1c7b
reginox <99781653+reginoxcf@users.noreply.github.com>
2024-03-23 09:58:58 +0700

    Update simulator.cpp

--- engine/simulator.cpp
+++ engine/simulator.cpp
@@ -8,6 +8,7 @@ struct stats{
 	string name;
 } teamStats[maxTeams];
 string command;
+int teamLength = 3, teamAttLength = 5, teamDefLength = 5;
 
 bool comp(stats &x, stats &y){
 	if(x.pts != y.pts) return x.pts > y.pts;
@@ -52,7 +53,9 @@ void print(string s, int sp){
 
 void printStandings(){
     sort(teamStats, teamStats+noOfTeams, comp);
-	cout << "#   | Club | MP  | W   | D   | L   | GF   | GA   | GD   | Pts   |\n";
+	cout << "#   | ";
+	print("Club", teamLength);
+	cout << "MP  | W   | D   | L   | GF   | GA   | GD   | Pts   |\n";
 	int rnk = 0;
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, 31);
@@ -61,7 +64,7 @@ void printStandings(){
 		if(rnk == 3) SetConsoleTextAttribute(hConsole, 15);
 		if(rnk == noOfTeams-1) SetConsoleTextAttribute(hConsole, 79);
 		print(to_string(rnk), 4);
-		print(x.name, 5);
+		print(x.name, teamLength);
 		print(to_string(x.mp), 4);
 		print(to_string(x.w), 4);
 		print(to_string(x.d), 4);
@@ -78,19 +81,29 @@ void printStandings(){
 }
 
 void simulateMatch(int id){
+	ofstream file("matches.txt", ios::app);
+	if(id % (noOfTeams/2) == 1) file << "\nMatchday " << (id+(noOfTeams/2)-1)/(noOfTeams/2) << ":\n";
     int ho = matches[id].first, aw = matches[id].second;
     pair<int, int> res = simulate(teamList[ho], teamList[aw]);
     cout << teamStats[ho].name << " " << res.first << " - " << res.second << " " << teamStats[aw].name << "\n";
+    file << teamStats[ho].name << " " << res.first << " - " << res.second << " " << teamStats[aw].name << "\n";
     updateStats(ho, res);
     updateStats(aw, {res.second, res.first});
 }
 
 int main(){
+	ofstream dele("matches.txt");
+	dele.close();
 	ifstream file("teams.txt");
 	// freopen("results.txt","w",stdout);
 	file >> noOfTeams;
 	for(int i = 0; i < noOfTeams; i++){
-		file >> teamStats[i].name >> teamList[i].att >> teamList[i].def;
+		getline(file, teamStats[i].name);
+		getline(file, teamStats[i].name);
+		teamLength = max(teamLength, (int)teamStats[i].name.size()+2);
+		file >> teamList[i].att >> teamList[i].def;
+		teamAttLength = max(teamAttLength, (int)to_string(teamList[i].att).size()+2);
+		teamDefLength = max(teamDefLength, (int)to_string(teamList[i].def).size()+2);
 	}
 	cout << "Teams imported from teams.txt\n";
 	file.close();
@@ -129,13 +142,22 @@ int main(){
                 cout << "Next match: " << teamStats[matches[id+1].first].name << " - " << teamStats[matches[id+1].second].name << "\n";
         }
         else if(command == "tl"){
+        	print("Club", teamLength);
+        	print("ATT", teamAttLength);
+        	print("DEF", teamDefLength);
+        	cout << "\n";
             for(int i = 0; i < noOfTeams; i++){
-                cout << teamStats[i].name << " " << teamList[i].att << " " << teamList[i].def << "\n";
+                print(teamStats[i].name, teamLength);
+                print(to_string(teamList[i].att), teamAttLength);
+                print(to_string(teamList[i].def), teamDefLength);
+                cout << "\n";
             }
         }
         if(id >= (noOfTeams)*(noOfTeams-1)) break;
         cout << ">>> ";
 	}
 	cout << "Final Standings:\n";
+	printStandings();
+	freopen("results.txt","w",stdout);
 	printStandings();
 }
